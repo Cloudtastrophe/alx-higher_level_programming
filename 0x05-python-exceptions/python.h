1606495c2c703ba09ee06a2f5bbe8f163bc5343f
Cloudtastrophe <ifiwil@yahoo.com>
2024-01-24 08:14:42 -0800

    personal commit

---
+++ 0x05-python-exceptions/python.h
@@ -0,0 +1,8 @@
+#ifndef MY_PYTHON_HEADER_H
+#define MY_PYTHON_HEADER_H
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif /* MY_PYTHON_HEADER_H */
